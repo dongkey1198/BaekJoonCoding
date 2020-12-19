e43c5afc246cc3511d204cbecc26d988396301b1
dongkey1198 <utdhkim94@gmail.com>
2020-12-19 21:52:45 +0900

    ct

---
+++ BJ/src/sort/Example9.java
@@ -0,0 +1,49 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Example9 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][]arr = new int[N][2];
+		
+		StringTokenizer st;
+		
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[1] == o2[1]) {
+					return Integer.compare(o1[0], o2[0]);
+				}
+				return Integer.compare(o1[1], o2[1]);
+			}
+		});
+		
+		br.close();
+		
+		for(int i = 0; i<arr.length; i++) {
+			for(int j = 0 ; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
